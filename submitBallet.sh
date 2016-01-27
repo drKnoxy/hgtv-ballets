a18cb4fd9d5c87aa8d11a92d32026a45a280744f
Adam Knox <adam.w.knox@gmail.com>
2016-01-27 16:15:52 -0500

    add diy network

--- submitBallet.sh
+++ submitBallet.sh
@@ -15,7 +15,14 @@ encoded_email=${email//@/%40}
 
 echo $bar #---------------------------------
 echo "hgtv.com part1"
-url1="https://submit.engagesciences.com/display/form/post?email=$encoded_email&apikey=a587a142-38bd-4a4b-ad68-939d090ab1d1&campaignObjectId=38967&ngx_remember_me=true&ngx_check_entry=true&isXHR=true"
+
+url1="https://submit.engagesciences.com/display/form/post?"\
+"email=$encoded_email"\
+"&apikey=a587a142-38bd-4a4b-ad68-939d090ab1d1"\
+"&campaignObjectId=38967"\
+"&ngx_remember_me=true"\
+"&ngx_check_entry=true"\
+"&isXHR=true"
 
 curl -X GET $url1 \
     -H "Accept: application/json, text/javascript, */*; q=0.01" \
@@ -27,8 +34,6 @@ curl -X GET $url1 \
     -H "Accept-Language: en-US,en;q=0.8" \
     -H "Cache-Control: no-cache" #-i # look at the response headers
 echo ""
-
-
 
 echo $bar #---------------------------------
 echo "hgtv.com part2"
@@ -50,4 +55,55 @@ curl 'https://submit.engagesciences.com/display/form/post' \
 
 # cleanup after ourselves
 rm tmp.txt
+echo ""
+
+
+
+echo $bar #---------------------------------
+echo "diynetwork.com part1"
+
+url2="https://submit.engagesciences.com/display/form/post?"\
+"email=$encoded_email"\
+"&apikey=a587a142-38bd-4a4b-ad68-939d090ab1d1"\
+"&campaignObjectId=39333"\
+"&ngx_remember_me=true"\
+"&ngx_check_entry=true"\
+"&isXHR=true"
+
+curl -X GET $url2 \
+    -H 'Pragma: no-cache' \
+    -H 'Origin: http://d.engagesciences.com' \
+    -H 'Accept-Encoding: gzip, deflate, sdch' \
+    -H 'Accept-Language: en-US,en;q=0.8,pt;q=0.6' \
+    -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36' \
+    -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+    -H 'Referer: http://d.engagesciences.com/display/container/dc/d45b9f69-9bce-4b06-96b1-ca8dd831da72/entry?source=diy' \
+    -H 'Connection: keep-alive' \
+    -H 'Cache-Control: no-cache' \
+    --compressed #-i # look at the response headers
+echo ""
+
+
+echo $bar #---------------------------------
+echo "diynetwork.com part2"
+
+# create a tmp form file with this email address
+sed "s/\${email}/$email/" <hgtv_form-data.txt >tmp2.txt
+
+curl 'https://submit.engagesciences.com/display/form/post' \
+    -H 'Pragma: no-cache' \
+    -H 'Origin: http://d.engagesciences.com' \
+    -H 'Accept-Encoding: gzip, deflate' \
+    -H 'Accept-Language: en-US,en;q=0.8,pt;q=0.6' \
+    -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36' \
+    -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryw7Cg8irjxgULitL0' \
+    -H 'Accept: */*' \
+    -H 'Cache-Control: no-cache' \
+    -H 'Referer: http://d.engagesciences.com/display/container/dc/d45b9f69-9bce-4b06-96b1-ca8dd831da72/entry?source=diy' \
+    -H 'Connection: keep-alive' \
+    --data-binary "@tmp2.txt" \
+    --compressed #-i # look at the response headers
+
+# cleanup after ourselves
+rm tmp2.txt
 echo ""
